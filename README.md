eb57c19e11ab89176813ea1e95dfbdf5e0c96df2
Edinson <fabianguzzman@gmail.com>
2024-07-20 12:22:07 -0500

    Add skills section and fixed images

---
+++ README.md
@@ -0,0 +1,7 @@
+
+<h1 align="center"> Su título aquí </h1>
+
+<p>parrafo normal </p>
+
+>[!NOTE]
+![Wizard](https://t3.ftcdn.net/jpg/05/83/22/42/360_F_583224250_s7QIQ0baa0ZF2Ol3Iw4XRAKY6t1yZZ5x.jpg)
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.header__menu__link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
